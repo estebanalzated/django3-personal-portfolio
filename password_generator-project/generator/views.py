6bea4d02bc16c6e286ef76a89b069818b7c4d4c7
alzatede <esteban.alzateduque@anz.com>
2022-03-11 16:26:25 +1100

    password generator finished

--- password_generator-project/generator/views.py
+++ password_generator-project/generator/views.py
@@ -10,10 +10,22 @@ def home(request):
     return render(request, 'generator/home.html')
 
 
+def about(request):
+    return render(request, 'generator/about.html')
+
+
 def password(request):
 
-    characters = list('abcdefghijklmnopqrstuvxyz')
-    length = 10
+    characters = list('')
+    if request.GET.get('lowercase'):
+        characters.extend(list('abcdefghijklmnopqrstuvxyz'))
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+    if request.GET.get('special'):
+        characters.extend(list('~!@#$%^&*()_+=-`[]\|}{;:/.,<>?'))
+    length = int(request.GET.get('length', 12))
     thepassword = ''
     for x in range(length):
         thepassword += random.choice(characters)
