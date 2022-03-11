a95129b4c61b5b0c8aacd2a321d8df767c3f3d9b
alzatede <esteban.alzateduque@anz.com>
2022-03-11 22:24:10 +1100

    new project

---
+++ personal_portfolio-project/portfolio/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='portfolio/images/')
+    url = models.URLField(blank=True)
+    # blank=True is a property to make this optional so url is  optional
