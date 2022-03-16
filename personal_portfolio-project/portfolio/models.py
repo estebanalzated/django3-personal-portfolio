f39e1d259888deac012e9e4b385d754a151c233a
alzatede <esteban.alzateduque@anz.com>
2022-03-16 13:44:47 +1100

    march15

--- personal_portfolio-project/portfolio/models.py
+++ personal_portfolio-project/portfolio/models.py
@@ -8,4 +8,4 @@ class Project(models.Model):
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='portfolio/images/')
     url = models.URLField(blank=True)
-    # blank=True is a property to make this optional so url is  optional
+    # blank=True is a property to make this optional so url is    optional
