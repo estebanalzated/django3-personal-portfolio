0048482b30d0897ff202b2cc7e4bbafd81235ae5
alzatede <esteban.alzateduque@anz.com>
2022-03-16 21:06:54 +1100

    mas

--- personal_portfolio-project/blog/models.py
+++ personal_portfolio-project/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
 # Create your models here.
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='blog/images/', blank=True)
+    pub_date = models.DateField(auto_now=True)
+    # url = models.URLField(blank=True)
+    # blank=True is a property to make this optional so url is    optional
